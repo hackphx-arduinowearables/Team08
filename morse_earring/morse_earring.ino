2ec21b77bd0a57d7f122b2022f0c6ce97db3f219
pyrobrooks <pyrobrooks@gmail.com>
2014-01-18 17:04:12 -0700

    morse flashing

--- morse_earring/morse_earring.ino
+++ morse_earring/morse_earring.ino
@@ -5,7 +5,7 @@
 
 CRGB leds[NUM_LEDS];
 
-int dotLength = 500; 
+int dotLength = 250; 
 
 void setup() {
     Serial.begin(9600);
@@ -13,6 +13,9 @@ void setup() {
     
     FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
+
+void morse(char c);
+
 void ledOn(int length)
 {
   int startTime = millis();
@@ -20,29 +23,29 @@ void ledOn(int length)
     {
     for (int i = 0; i < NUM_LEDS; i++)
     {
-        leds[i] = CRGB::Blue;
-        FastLED.show();
-        leds[i] = CRGB::Black;
-        //delay (5);
+      leds[i] = CRGB::Blue;
+      FastLED.show();
+      leds[i] = CRGB::Black;
+      //delay (5);
     }//lights LEDs
   }
   delay(dotLength);
 }
+
 void dot()
 {
   ledOn(dotLength);
 }
+
 void dash()
 {
   ledOn(3*dotLength);
  //3 times as long 
 }
+
 void loop()
 {
-  dot();
-  dash();
-}
-  /*String content = "";
+  String content = "";
   char character;
   while (Serial1.available())
   {
@@ -57,11 +60,154 @@ void loop()
     int length = content.length();
     for (int i = 0; i < length; i++)
     {
-      Serial.println(content[i]);
+      morse(content[i]);
     }
   }
-  
-  // Morse shit goes here.
-  
-  
-}*/
+}
+
+void morse(char c)
+{
+  switch (c)
+  {
+    case 'a':
+      dot();
+      dash();
+      break;
+    case 'b':
+      dash();
+      dot();
+      dot();
+      dot();
+      break;
+    case 'c':
+      dash();
+      dot();
+      dash();
+      dot();
+      break;
+    case 'd':
+      dash();
+      dot();
+      dot();
+      break;
+    case 'e':
+      dot();
+      break;
+    case 'f':
+      dot();
+      dot();
+      dash();
+      dot();
+      break;
+    case 'g':
+      dash();
+      dash();
+      dot();
+      break;
+    case 'h':
+      dot();
+      dot();
+      dot();
+      dot();
+      break;
+    case 'i':
+      dot();
+      dot();
+      break;
+    case 'j':
+      dot();
+      dash();
+      dash();
+      dash();
+      break;
+    case 'k':
+      dash();
+      dot();
+      dash();
+      break;
+    case 'l':
+      dot();
+      dash();
+      dot();
+      dot();
+      break;
+    case 'm':
+      dash();
+      dash();
+      break;
+    case 'n':
+      dash();
+      dot();
+      break;
+    case 'o':
+      dash();
+      dash();
+      dash();
+      break;
+    case 'p':
+      dot();
+      dash();
+      dash();
+      dot();
+      break;
+    case 'q':
+      dash();
+      dash();
+      dot();
+      dash();
+      break;
+    case 'r':
+      dot();
+      dash();
+      dot();
+      break;
+    case 's':
+      dot();
+      dot();
+      dot();
+      break;
+    case 't':
+      dash();
+      break;
+    case 'u':
+      dot();
+      dot();
+      dash();
+      break;
+    case 'v':
+      dot();
+      dot();
+      dot();
+      dash();
+      break;
+    case 'w':
+      dot();
+      dash();
+      dash();
+      break;
+    case 'x':
+      dash();
+      dot();
+      dot();
+      dash();
+      break;
+    case 'y':
+      dash();
+      dot();
+      dash();
+      dash();
+      break;
+    case 'z':
+      dash();
+      dash();
+      dot();
+      dot();
+      break;
+    case ' ':
+      delay(dotLength * 3);
+      break;
+    default:
+      break;
+  }
+}
+
